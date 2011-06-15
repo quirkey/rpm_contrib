c42e086f9774dcad724d2a3e369e50a4b8ee845c
Aaron Quint <aaron@quirkey.com>
2011-06-15 12:28:29 -0400

    class_name is not a method, use to_s on a class

--- lib/rpm_contrib/instrumentation/resque.rb
+++ lib/rpm_contrib/instrumentation/resque.rb
@@ -4,7 +4,7 @@ module Resque
       include NewRelic::Agent::Instrumentation::ControllerInstrumentation
 
       def around_perform_with_monitoring(*args)
-        perform_action_with_newrelic_trace(:name => 'perform', :class_name => class_name, :category => 'OtherTransaction/ResqueJob') do
+        perform_action_with_newrelic_trace(:name => 'perform', :class_name => to_s, :category => 'OtherTransaction/ResqueJob') do
           yield(*args)
         end
 
@@ -31,4 +31,3 @@ module RPMContrib
     end
   end
 end if defined?(::Resque) and not NewRelic::Control.instance['disable_resque']
-
